03ad10e22fc6a3fc0750c25ac74fa4497b8c9d29
khbsd <kayabeans@proton.me>
2026-07-31 19:50:51 -0500

    fix: okay actually fix things

--- senshabot.sh
+++ senshabot.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/sh
+cd /home/senshabot/host/SenshaBot &&
 git fetch https://github.com/SatanModding/SenshaBot &&
 git pull git fetch https://github.com/SatanModding/SenshaBot &&
 python bot.py
