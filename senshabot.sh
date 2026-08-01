5b14ef47ba62d9a46af842f14a9ccbea8d269f53
khbsd <kayabeans@proton.me>
2026-07-31 19:16:21 -0500

    fix: all that shit

---
+++ senshabot.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/sh
+git fetch https://github.com/SatanModding/SenshaBot &&
+git pull git fetch https://github.com/SatanModding/SenshaBot &&
+python bot.py
--- utils/senshabot.sh
+++
@@ -1,8 +0,0 @@
-!# /bin/bash
-echo "checking git..."
-&&
-exec python3 /home/kaya/host/discord/SenshaBot/utils/git.py
-;
-echo "starting bot..."
-&&
-exec python3 /home/kaya/host/discord/SenshaBot/bot.py
